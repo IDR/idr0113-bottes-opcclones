99ee6ce6248b2f3b16c6408815ff63eba766040b
Dominik Lindner <d.lindner@dundee.ac.uk>
2021-07-30 14:52:43 +0100

    Add remove option to add_notebook script

--- scripts/add_notebook_link.py
+++ scripts/add_notebook_link.py
@@ -12,14 +12,20 @@ REF_URL = "https://binder.bioimagearchive.org/v2/gh/IDR/idr0113-bottes-opcclones
 
 
 # Load the images in the project
-def load_data(conn, project_id):
+def load_data(conn, project_id, delete):
+    to_delete = []
     # load all the images in the project
     project = conn.getObject("Project", project_id)
     for dataset in project.listChildren():
         for image in dataset.listChildren():
             # Check if image has ROIs
-            if image.getROICount() > 0:
-                add_link(conn, image)
+            if (image.getROICount() > 0):
+                if (image.getAnnotation(ns=NAMESPACE) is None) and not delete:
+                    add_link(conn, image)
+                elif (image.getAnnotation(ns=NAMESPACE) is not None) and delete:
+                    to_delete.append(image.getAnnotation(ns=NAMESPACE).getId())
+    if to_delete:
+        conn.deleteObjects('Annotation', to_delete, wait=True)
 
 
 def add_link(conn, image):
@@ -36,13 +42,15 @@ def add_link(conn, image):
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('id')
+    parser.add_argument("-r",  "--remove", action="store_true", default=False,
+    help="Remove the annotations")
     args = parser.parse_args(args)
     # Create a connection and load the images in the specified project
     with omero.cli.cli_login() as c:
         try:
             conn = omero.gateway.BlitzGateway(client_obj=c.get_client())
             conn.c.enableKeepAlive(60)
-            load_data(conn, args.id)
+            load_data(conn, args.id, args.remove)
         finally:
             conn.close()
             print("done")
@@ -50,3 +58,4 @@ def main(args):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
